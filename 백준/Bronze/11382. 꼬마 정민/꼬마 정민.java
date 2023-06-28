4d618b7aacc69471285f5cf91e92fb8bbbffddab
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2023-06-28 23:50:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        br.close();
+
+        bw.write(String.valueOf(a + b + c));
+        bw.flush();
+        bw.close();
+    }
+}
