a45fcdd65ddb4022245c0b2f19b9ac8a6e88eb4c
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2023-07-11 02:19:00 +0900

    [Silver IV] Title: 제로, Time: 244 ms, Memory: 23980 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,29 +1,27 @@
 import java.io.*;
+import java.util.Stack;
 
 public class Main {
-
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int k = Integer.parseInt(br.readLine());
-        int[] arr = new int[k];
+        Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < k; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if (arr[i] == 0) {
-                for (int j = i; j >= 0; j--) {
-                    if (arr[j] != 0) {
-                        arr[j] = 0;
-                        break;
-                    }
-                }
-            }
+            int tmp = Integer.parseInt(br.readLine());
+            if (tmp != 0) {
+                stack.push(tmp);
+            } else {
+                stack.pop();
+            } 
         }
         int sum = 0;
-        for (int x : arr) {
-            sum += x;
+        for (int x : stack) {
+            sum += x; 
         }
         bw.write(String.valueOf(sum));
         bw.flush();
