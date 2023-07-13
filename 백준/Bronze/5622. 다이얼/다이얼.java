2c0c79481cee2511bd0cc6eea41b8f0502bd5c70
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2023-07-13 23:27:15 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String[] dial = {"", "", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ", ""};
+        
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            for (int j = 0; j < 12; j++) {
+                if (dial[j].contains(String.valueOf(c))) {
+                    sum += j;
+                }
+            }
+        }
+        System.out.print(sum);
+    }
+}
