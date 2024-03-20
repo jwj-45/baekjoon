2d19ef9d8aa2eb4fa40ec890eaabbfe9e2dd219c
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2024-03-20 12:53:01 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -3,22 +3,6 @@ import java.util.*;
 
 public class Main {
 	
-	private int[] drow = {1, -1, 0, 0};
-	private int[] dcol = {0, 0, 1, -1};
-	private static int cnt = 0;
-	
-	public void dfs(int row, int col, int[][] dp) {
-		for(int i = 0; i < 4; i++) {
-			int nRow = row + drow[i];
-			int nCol = col + dcol[i];
-			if(nRow >= 0 && nRow < 101 && nCol >= 0 && nCol < 101 && dp[nRow][nCol] == 1) {
-				cnt++;
-				dp[nRow][nCol] = 0;
-				dfs(nRow, nCol, dp);
-			}
-		}
-	}
-	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,17 +19,17 @@ public class Main {
 					dp[j][k] = 1;
 				}
 			}
-		}
+        }
 		
-		Main main = new Main();
-				
+		int cnt = 0;
 		for(int j = 1; j <= 100; j++) {
 			for(int k = 1; k <= 100; k++) {
 				if(dp[j][k] == 1) {
-					main.dfs(j, k, dp);
+					cnt++;
 				}
 			}
 		}
 		System.out.println(cnt);
 	}
 }
+
