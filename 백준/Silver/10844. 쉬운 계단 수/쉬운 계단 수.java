40a7a7b8ca26832e24b4bb435d7f6348088558bc
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2025-03-19 11:38:06 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,41 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        long[][] dp = new long[n + 1][10];
-
-        long mod = 1_000_000_000;
-
-        // 초기값 설정
-        // dp[1][0] = 0; // 없어도 됨
-        for (int i = 1; i <= 9; i++) {
-            dp[1][i] = 1;
-        }
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = 0; j <= 9; j++) {
-                if (j == 0)
-                    dp[i][j] = (dp[i - 1][j + 1]) % mod;
-                else if (j == 9)
-                    dp[i][j] = (dp[i - 1][j - 1]) % mod;
-                else
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
-
-            }
-        }
-
-        long sum = 0;
-        for (int i = 0; i <= 9; i++) {
-            sum += dp[n][i];
-        }
-        System.out.println(sum % mod);
-
-
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int mod = 1000000000;
+		
+		long[][] dp = new long[n + 1][10];
+		
+		for(int i = 0; i <= 9; i++) {
+			dp[1][i] = 1;
+		}
+		
+		for(int i = 2; i <= n; i++) {
+			for(int j = 0; j <= 9; j++) {
+				if(j == 0) {
+					dp[i][j] = dp[i - 1][j + 1]%mod;
+				}
+				else if(j == 9) {
+					dp[i][j] = dp[i - 1][j - 1]%mod;
+				}
+				else {
+					dp[i][j] = (dp[i - 1][j + 1]%mod + dp[i - 1][j - 1]%mod)%mod;
+				}
+			}
+		}
+		int sum = 0;
+		for(int i = 1; i <= 9; i++) {
+			sum += dp[n][i];
+			sum%=mod;
+		}
+		System.out.println(sum%=mod);
+	}
 }
