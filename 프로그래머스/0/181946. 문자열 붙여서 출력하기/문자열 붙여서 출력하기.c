3f375d0fee96c73db4403b52f813005bbabdd574
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2025-04-14 21:36:02 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 4.11 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.c
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.c
@@ -6,6 +6,8 @@ int main(void) {
     char s1[LEN_INPUT1];
     char s2[LEN_INPUT2];
     scanf("%s %s", s1, s2);
-    printf("%s%s", s1, s2);
+    
+    strcat(s1, s2);
+    printf("%s", s1);
     return 0;
 }
